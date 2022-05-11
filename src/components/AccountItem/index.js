560aec6e69a65bf639c7d2d67db3eee821705487
khoahd7621 <hoangdangkhoa7621@gmail.com>
2022-05-11 18:44:19 +0700

    Update header UI #2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,29 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src={
+                    'https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/8dc676096a0f89fe7845508f9c5b607a~c5_100x100.jpeg?x-expires=1652439600&x-signature=K3d6zWUM12UCiBq9anvruZvQ8E8%3D'
+                }
+                alt="Avatar"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Hoang Dang Khoa</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>khoahd7621</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
