3e5c042ce63d0977e9540fe10e19c4becb2b5453
Hoang Dang Khoa <hoangdangkhoa7621@gmail.com>
2022-06-13 13:08:07 +0700

    Fixed too many index.js files

---
+++ src/layouts/components/Header/Header.js
@@ -0,0 +1,155 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react';
+import { Link } from 'react-router-dom';
+import 'tippy.js/dist/tippy.css';
+
+import config from '~/config';
+import Button from '~/components/Button';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+import Menu from '~/components/Popper/Menu';
+import {
+    GetCoinIcon,
+    InboxIcon,
+    KeyboardIcon,
+    LanguageIcon,
+    LogoutIcon,
+    MessageIcon,
+    ProfileIcon,
+    QuestionIcon,
+    SettingIcon,
+    UploadIcon,
+} from '~/components/Icons';
+import Image from '~/components/Image';
+import Search from '../Search';
+
+const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+    {
+        icon: <LanguageIcon />,
+        title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    type: 'language',
+                    code: 'en',
+                    title: 'English',
+                },
+                {
+                    type: 'language',
+                    code: 'vi',
+                    title: 'Tiếng Việt',
+                },
+            ],
+        },
+    },
+    {
+        icon: <QuestionIcon />,
+        title: 'Feedback and help',
+        to: '/feedback',
+    },
+    {
+        icon: <KeyboardIcon />,
+        title: 'Keybord shortcuts',
+    },
+];
+
+function Header() {
+    const currentUser = true;
+
+    // Handle logic
+    const handleMenuChange = (menuItem) => {
+        switch (menuItem.type) {
+            case 'language':
+                // Handle change language
+                break;
+            default:
+                break;
+        }
+    };
+
+    const userMenu = [
+        {
+            icon: <ProfileIcon />,
+            title: 'View profile',
+            to: '/@hoaa',
+        },
+        {
+            icon: <GetCoinIcon />,
+            title: 'Get coins',
+            to: '/coin',
+        },
+        {
+            icon: <SettingIcon />,
+            title: 'Settings',
+            to: '/settings',
+        },
+        ...MENU_ITEMS,
+        {
+            icon: <LogoutIcon />,
+            title: 'Log out',
+            to: '/logout',
+            separate: true,
+        },
+    ];
+
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <Link to={config.routes.home} className={cx('logo-link')}>
+                    <img src={images.logo} alt="Tiktok" />
+                </Link>
+
+                <Search />
+
+                <div className={cx('actions')}>
+                    {currentUser ? (
+                        <>
+                            <Tippy delay={[0, 50]} content={'Upload video'} placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <UploadIcon />
+                                </button>
+                            </Tippy>
+                            <Tippy delay={[0, 50]} content={'Message'} placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <MessageIcon />
+                                </button>
+                            </Tippy>
+                            <Tippy delay={[0, 50]} content={'Inbox'} placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <InboxIcon />
+                                    <span className={cx('badge')}>12</span>
+                                </button>
+                            </Tippy>
+                        </>
+                    ) : (
+                        <>
+                            <Button text>Upload</Button>
+                            <Button primary>Log in</Button>
+                        </>
+                    )}
+
+                    <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
+                        {currentUser ? (
+                            <Image
+                                src="https://1.bigdata-vn.com/wp-content/uploads/2021/11/1635776930_484_199-Anh-Avatar-dep-cho-con-gai-xinh-xan-ca.jpg"
+                                className={cx('user-avatar')}
+                                alt="Nguyen Van A"
+                            />
+                        ) : (
+                            <button className={cx('more-btn')}>
+                                <FontAwesomeIcon icon={faEllipsisVertical} />
+                            </button>
+                        )}
+                    </Menu>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
